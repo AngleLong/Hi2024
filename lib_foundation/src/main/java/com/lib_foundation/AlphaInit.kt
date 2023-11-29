24b237f2872c85d26fc702f9266e7266164a5c2e
hejinlong <753355530@qq.com>
2023-11-29 16:11:39 +0800

    差一个类没提交

--- lib_foundation/src/main/java/com/lib_foundation/AlphaInit.kt
+++ lib_foundation/src/main/java/com/lib_foundation/AlphaInit.kt
@@ -1,31 +1,13 @@
 package com.lib_foundation
 
 import android.app.Application
-import com.orhanobut.logger.DiskLogAdapter
-import com.orhanobut.logger.FormatStrategy
-import com.orhanobut.logger.Logger
-import com.orhanobut.logger.PrettyFormatStrategy
+import com.lib_foundation.logger.LoggerManager
 import com.lib_foundation.logger.MyCsvFormatStrategy
 
 
 class AlphaInit : InitModelProject {
     override fun init(application: Application) {
-
-//        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
-//            .showThreadInfo(false)
-//            .methodCount(0)
-//            .methodOffset(7)
-//            .tag("YouAngle")
-//            .build()
-
-        Logger.addLogAdapter(
-            DiskLogAdapter(
-                MyCsvFormatStrategy
-                    .newBuilder()
-                    .context(application)
-                    .tag(application.packageName)
-                    .build()
-            )
-        )
+        //初始化日志类
+        LoggerManager.initLogger(application)
     }
 }
